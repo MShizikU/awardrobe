f5d0542e6851ff6c1f4db9c6120e63b9b3c40b52
Stanislav Sidorov <sidorovstasd7@gmail.com>
2024-04-09 18:42:52 +0300

    Spring Proj Init

---
+++ app/src/main/java/ru/mirea/ikbo2021/sidorov/awardrobe/AwardrobeApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.ikbo2021.sidorov.awardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwardrobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwardrobeApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/ru/mirea/ikbo2021/sidorov/awardrobe/AwardrobeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.ikbo2021.sidorov.awardrobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwardrobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
